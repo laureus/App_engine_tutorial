024623f162708b6038c9b4fd001a0f7838e09149
Yuqing Luo <laureus31@126.com>
2011-10-29 13:13:31 +1100

    add jsp
    sign off by me

--- src/com/tutorial/appengine/GuestbookServlet.java
+++ src/com/tutorial/appengine/GuestbookServlet.java
@@ -2,7 +2,7 @@ package com.tutorial.appengine;
 
 import java.io.IOException;
 import javax.servlet.http.*;
-import com.google.appengine.api.users.*;
+//import com.google.appengine.api.users.*;
 
 @SuppressWarnings("serial")
 public class GuestbookServlet extends HttpServlet {
@@ -10,7 +10,18 @@ public class GuestbookServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		
-		UserService userService = UserServiceFactory.getUserService();
+		String userPath = req.getServletPath();
+		
+		if(userPath.equals("/guestbook")){
+			
+			try{
+				req.getRequestDispatcher("/jsp/guestbook.jsp").forward(req, resp);
+			}catch(Exception ex){
+				ex.printStackTrace();
+			}
+			
+		}
+		/*UserService userService = UserServiceFactory.getUserService();
 		User user = userService.getCurrentUser();
 		
 		if(user != null){
@@ -19,7 +30,7 @@ public class GuestbookServlet extends HttpServlet {
 		} else{
 			resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
 		}
-		
+		*/
 		//resp.setContentType("text/plain");
 		//resp.getWriter().println("Hello, this is guestbook!");
 	}
---
+++ war/jsp/guestbook.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ page import="com.google.appengine.api.users.User" %>
+<%@ page import="com.google.appengine.api.users.UserService" %>
+<%@ page import="com.google.appengine.api.users.UserServiceFactory" %>
+
+<html>
+  <body>
+
+<%
+    UserService userService = UserServiceFactory.getUserService();
+    User user = userService.getCurrentUser();
+    if (user != null) {
+%>
+<p>Hello, <%= user.getNickname() %>! (You can
+<a href="<%= userService.createLogoutURL(request.getRequestURI()) %>">sign out</a>.)</p>
+<%
+    } else {
+%>
+<p>Hello!
+<a href="<%= userService.createLoginURL(request.getRequestURI()) %>">Sign in</a>
+to include your name with greetings you post.</p>
+<%
+    }
+%>
+
+  </body>
+</html>
